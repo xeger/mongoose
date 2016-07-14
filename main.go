e41534fe868ee6471b0d43cdf0b35dcee735343b
Tony Spataro <tony@rightscale.com>
2016-07-13 20:25:28 -0700

    Add CLI flags

--- main.go
+++ main.go
@@ -1,32 +1,60 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/xeger/mongoose/gen"
 	"github.com/xeger/mongoose/parse"
 )
 
+var mockPackage = flag.String("mock", "testify", "framework: testify,...")
+var mockOutput = flag.String("out", ".", "dir/subdir for mock files (- for stdout)")
+
+func testify() bool {
+	return strings.Index(*mockPackage, "testify") >= 0
+}
+
+func placer() gen.Placer {
+	return &gen.PackagePlacer{FilePerInterface: testify()}
+}
+
+func writer() gen.Writer {
+	if *mockOutput == "-" {
+		return &gen.StdoutWriter{}
+	}
+	return &gen.FileWriter{}
+}
+
+func renderer() gen.Renderer {
+	if testify() {
+		return gen.NewTestifyRenderer()
+	}
+	panic("not implemented")
+}
+
 func main() {
+	flag.Parse()
+
+	placer := placer()
+	writer := writer()
+
 	pkg, err := parse.NewPackage(os.Args[1])
 	if err != nil {
-		fmt.Println("// Parse failure:")
-		fmt.Println("//   ", err)
+		fmt.Println("Parse failure:", err)
 		os.Exit(1)
 	}
 
-	placer := gen.PackagePlacer{}
 	placed := map[string][]parse.Interface{}
 	for _, intf := range pkg.Interfaces {
 		place := placer.Place(pkg.Dir, &intf)
 		placed[place] = append(placed[place], intf)
 	}
 
-	writer := gen.FileWriter{}
-
 	for path, intfs := range placed {
-		rend := gen.NewTestifyRenderer()
+		rend := renderer()
 		source, err := rend.Render(pkg, intfs)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Code generation failure:", err)
